9a558b4eaceb655d1505c6c8817cad8ac9d79aa2
Steve Mattsen <mattsensteve@gmail.com>
2024-05-21 00:13:36 -0400

    Added direcitonal animations and fixed some quirks of input

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,25 +1,45 @@
 import { bbox, vec2 } from "./Vec2";
 import { Obj } from "./Obj";
 import { Img, Animation, Frame } from "./Sprites";
-import "./World"
+import "./World";
 
 var inputState: { [id: string]: boolean } = {};
 
-window.onkeydown = e => inputState[e.key] = true;
-window.onkeyup = e => inputState[e.key] = false;
+window.onkeydown = e => {
+	if (e.key == 'Tab') {
+		e.preventDefault();
+	}
+	inputState[e.key.toLowerCase()] = true;
+}
+window.onkeyup = e => {
+	inputState[e.key.toLowerCase()] = false;
+}
+window.onblur = e => {
+	inputState = {};
+}
 
 function tick() {
 	let move = new vec2(
-		(inputState.ArrowRight || inputState.d ? 1 : 0) - (inputState.ArrowLeft || inputState.a ? 1 : 0),
-		(inputState.ArrowDown || inputState.s ? 1 : 0) - (inputState.ArrowUp || inputState.w ? 1 : 0)
+		(inputState.arrowright || inputState.d ? 1 : 0) - (inputState.arrowleft || inputState.a ? 1 : 0),
+		(inputState.arrowdown || inputState.s ? 1 : 0) - (inputState.arrowup || inputState.w ? 1 : 0)
 	);
-	let speed = inputState.Shift ? 8 : 2;
+	let speed = inputState.shift ? 8 : 3;
 	//Normalize
 	move = move.normalize();
 	let plyr = Obj.store['player'];
-	plyr.animState = move.length() > 0 ? (
-		inputState.Shift ? 'run' : 'walk'
-	) : 'idle';
+	plyr.animState = 'idle';
+	if (move.length() > 0) {
+		plyr.animState = inputState.shift ? 'run' : 'walk'
+	}
+	if (move.x > 0) {
+		plyr.animState += "_right";
+	} else if (move.x < 0) {
+		plyr.animState += "_left";
+	} else if (move.y > 0) {
+		plyr.animState += "_down";
+	} else if (move.y < 0) {
+		plyr.animState += "_up";
+	}
 	plyr.tickAnimFrame();
 	plyr.pos.x += (move.x ?? 0) * speed;
 	plyr.pos.y += (move.y ?? 0) * speed;
@@ -56,10 +76,10 @@ function draw() {
 		);
 		ctx.fillStyle = "black";
 		ctx.strokeRect(
-			Math.round(obj.pos.x),
-			Math.round(obj.pos.y),
-			Math.round(obj.size.x),
-			Math.round(obj.size.y),
+			Math.floor(obj.pos.x),
+			Math.floor(obj.pos.y),
+			Math.floor(obj.size.x),
+			Math.floor(obj.size.y),
 		);
 		ctx.font = "24px Roboto"
 		ctx.fillText(
@@ -77,9 +97,22 @@ function draw() {
 			window width: ${window.innerWidth}`,
 			0,24
 		);
+		let count = 0;
+		ctx.textAlign = "right"
+		ctx.textBaseline = "top"
+		Object.keys(inputState).forEach(v => {
+			if (!inputState[v]) {
+				return;
+			}
+			ctx.fillText(
+				v,
+				window.innerWidth,
+				count++ * 24,
+			)
+		});
 	});
 }
 
-let refresh = 60;
+let refresh = 59.67;
 let drawThread = setInterval(draw, 1000 / refresh);
 let gameThread = setInterval(tick, 1000 / refresh);
--- src/World.ts
+++ src/World.ts
@@ -14,7 +14,7 @@ let anims: { [id: string]: Animation } = {
 		new Frame(spritesheet_link, xSize * 2, 0, xSize, ySize, frameTime),
 		new Frame(spritesheet_link, xSize * 1, 0, xSize, ySize, frameTime),
 	]),
-	'walk': new Animation([
+	'walk_right': new Animation([
 		new Frame(spritesheet_link, xSize * 9 + 4, ySize * 7, xSize, ySize, frameTime),
 		new Frame(spritesheet_link, xSize * 8 + 7, ySize * 7, xSize - 8, ySize, frameTime),
 		new Frame(spritesheet_link, xSize * 6 + 0, ySize * 7, xSize, ySize, frameTime),
@@ -23,26 +23,95 @@ let anims: { [id: string]: Animation } = {
 		new Frame(spritesheet_link, xSize * 3 + 6, ySize * 7, xSize - 7, ySize, frameTime),
 		new Frame(spritesheet_link, xSize * 1 + 2, ySize * 7, xSize, ySize, frameTime),
 		new Frame(spritesheet_link, xSize * 0 + 0, ySize * 7, xSize, ySize, frameTime),
+	]),
+	'walk_left': new Animation([
+		new Frame(spritesheet_link, xSize * 0 + 0, ySize * 5, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 1 + 2, ySize * 5, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 3 + 6, ySize * 5, xSize - 7, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 4 + 6, ySize * 5, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 5 + 0, ySize * 5, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 6 + 0, ySize * 5, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 8 + 7, ySize * 5, xSize - 8, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 9 + 4, ySize * 5, xSize, ySize, frameTime),
+	]),
+	'walk_down': new Animation([
+		new Frame(spritesheet_link, xSize * 0 + 0, ySize * 4, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 1 + 2, ySize * 4, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 3 + 6, ySize * 4, xSize - 7, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 4 + 6, ySize * 4, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 5 + 0, ySize * 4, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 6 + 0, ySize * 4, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 8 + 7, ySize * 4, xSize - 8, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 9 + 4, ySize * 4, xSize, ySize, frameTime),
+	]),
+	'walk_up': new Animation([
+		new Frame(spritesheet_link, xSize * 0 + 0, ySize * 6, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 1 + 2, ySize * 6, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 3 + 6, ySize * 6, xSize - 7, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 4 + 6, ySize * 6, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 5 + 0, ySize * 6, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 6 + 0, ySize * 6, xSize, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 8 + 7, ySize * 6, xSize - 8, ySize, frameTime),
+		new Frame(spritesheet_link, xSize * 9 + 4, ySize * 6, xSize, ySize, frameTime),
 	])
 };
-anims['run'] = new Animation([
-	anims['walk'].frames[0],
-	anims['walk'].frames[1],
+anims['run_right'] = new Animation([
+	anims['walk_right'].frames[0],
+	anims['walk_right'].frames[1],
 	new Frame(spritesheet_link, xSize * 2, ySize * 7, xSize, ySize, frameTime * 1.75),
-	anims['walk'].frames[2],
-	anims['walk'].frames[3],
-	anims['walk'].frames[4],
-	anims['walk'].frames[5],
+	anims['walk_right'].frames[2],
+	anims['walk_right'].frames[3],
+	anims['walk_right'].frames[4],
+	anims['walk_right'].frames[5],
 	new Frame(spritesheet_link, xSize * 7, ySize * 7, xSize, ySize, frameTime * 1.75),
-	anims['walk'].frames[6],
-	anims['walk'].frames[7],
+	anims['walk_right'].frames[6],
+	anims['walk_right'].frames[7],
 ]);
+anims['run_left'] = new Animation([
+	anims['walk_left'].frames[0],
+	anims['walk_left'].frames[1],
+	new Frame(spritesheet_link, xSize * 2, ySize * 5, xSize, ySize, frameTime * 1.75),
+	anims['walk_left'].frames[2],
+	anims['walk_left'].frames[3],
+	anims['walk_left'].frames[4],
+	anims['walk_left'].frames[5],
+	new Frame(spritesheet_link, xSize * 7, ySize * 5, xSize, ySize, frameTime * 1.75),
+	anims['walk_left'].frames[6],
+	anims['walk_left'].frames[7],
+]);
+
+anims['run_down'] = new Animation([
+	anims['walk_down'].frames[0],
+	anims['walk_down'].frames[1],
+	new Frame(spritesheet_link, xSize * 2, ySize * 4, xSize, ySize, frameTime * 1.75),
+	anims['walk_down'].frames[2],
+	anims['walk_down'].frames[3],
+	anims['walk_down'].frames[4],
+	anims['walk_down'].frames[5],
+	new Frame(spritesheet_link, xSize * 7, ySize * 4, xSize, ySize, frameTime * 1.75),
+	anims['walk_down'].frames[6],
+	anims['walk_down'].frames[7],
+]);
+
+anims['run_up'] = new Animation([
+	anims['walk_up'].frames[0],
+	anims['walk_up'].frames[1],
+	new Frame(spritesheet_link, xSize * 2, ySize * 6, xSize, ySize, frameTime * 1.75),
+	anims['walk_up'].frames[2],
+	anims['walk_up'].frames[3],
+	anims['walk_up'].frames[4],
+	anims['walk_up'].frames[5],
+	new Frame(spritesheet_link, xSize * 7, ySize * 6, xSize, ySize, frameTime * 1.75),
+	anims['walk_up'].frames[6],
+	anims['walk_up'].frames[7],
+]);
+
 const player = new Obj(
 	'player', 
 	Img.store['spritesheet_link'], 
-	new vec2(xSize - 4, ySize), 
+	new vec2(xSize - 1, ySize), 
 	new vec2(
-		(window.innerWidth - xSize -4) / 2,
+		(window.innerWidth - xSize -1) / 2,
 		(window.innerHeight - ySize) / 2
 	), 
 	anims
