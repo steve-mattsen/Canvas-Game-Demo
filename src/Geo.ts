c5f8d62f04d81250343c5e0743de20396bb234f8
Steve Mattsen <mattsensteve@gmail.com>
2024-05-24 11:38:04 -0400

    clean: Auto formatting

--- src/Geo.ts
+++ src/Geo.ts
@@ -36,7 +36,7 @@ export class vec3 extends vec2 {
 	y = 0;
 	z = 0;
 	constructor(x: number, y: number, z: number) {
-		super(x,y);
+		super(x, y);
 		this.z = z;
 	}
 }
@@ -79,9 +79,9 @@ export class bbox {
 		return Math.sqrt(sumProduct);
 	}
 	contains(point: vec2) {
-		if (point.x < this.topLeft.x 
-			|| point.x > this.bottomRight.x 
-			|| point.y < this.topLeft.y 
+		if (point.x < this.topLeft.x
+			|| point.x > this.bottomRight.x
+			|| point.y < this.topLeft.y
 			|| point.y > this.bottomRight.y
 		) {
 			return false;
@@ -90,9 +90,9 @@ export class bbox {
 	}
 }
 
-export function vec(x: number, y:number, z:number = null) {
+export function vec(x: number, y: number, z: number = null) {
 	if (z == null) {
-		return new vec2(x,y);
+		return new vec2(x, y);
 	}
-	return new vec3(x,y,z);
+	return new vec3(x, y, z);
 }
