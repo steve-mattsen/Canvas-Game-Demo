8dc44c77503f7ebf541b6f2c4ae26ea6854f8f9f
Steve Mattsen <mattsensteve@gmail.com>
2024-05-26 04:15:27 -0500

    rework: Fixed the tests I couldn't figure out.

--- src/Geo.test.ts
+++ src/Geo.test.ts
@@ -5,8 +5,8 @@ let dataSet = [
 	[0, 1],
 	[1, 0],
 	[-1, 0],
-	[-1, Number.MAX_VALUE],
-	[Number.MAX_VALUE, -1],
+	[-1, Number.MAX_SAFE_INTEGER],
+	[Number.MAX_SAFE_INTEGER, -1],
 	[0, 1],
 ];
 
@@ -15,25 +15,22 @@ let lengthDataSet = [
 	[0, 5, 5],
 ];
 
-test.each(dataSet)('a vector will have correct dimensions: (%p, %p)', (d) => {
-	let v = new Vec2(d[0], d[1]);
-	expect(v.x).toBe(d[0]);
-	expect(v.y).toBe(d[1]);
+test.each(dataSet)('a vector will have correct dimensions: (%p, %p)', (x, y) => {
+	let v = new Vec2(x, y);
+	expect(v.x).toBe(x);
+	expect(v.y).toBe(y);
 });
-test.each(dataSet)('vec() returns Vec2: (%p, %p)', (d) => {
-	let v = vec(d[0], d[1]);
+test.each(dataSet)('vec() returns Vec2: (%p, %p)', (x, y) => {
+	let v = vec(x, y);
 	expect(v).toBeInstanceOf(Vec2);
 });
 
-// test.each(lengthDataSet)('dicks', (d) => {
-// 	expect(d[0]).not.toBe(d[1]);
-// })
-// test.each(lengthDataSet)('vector lengths are calculated properly: (%p, %p)', (d) => {
-// 	let v = vec(d[0], d[1]);
-// 	expect(v.length()).toBe(d[2]);
-// });
-// test.each(dataSet)('vector normalize() will have length 1: (%p, %p)', (d) => {
-// 	let v = vec(d[0], d[1])
-// 	v = v.normalize();
-// 	expect(v.length()).toBeCloseTo(1);
-// });
+test.each(lengthDataSet)('vector lengths are calculated properly: (%p, %p)', (x, y, r) => {
+	let v = vec(x, y);
+	expect(v.length()).toBe(r);
+});
+test.each(dataSet)('vector normalize() will have length 1: (%p, %p)', (x, y) => {
+	let v = vec(x, y);
+	v = v.normalize();
+	expect(v.length()).toBeCloseTo(1);
+});
