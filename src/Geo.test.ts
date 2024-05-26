3bdaec8d755c9abbd9c710a7c9c836474bc91837
Steve Mattsen <mattsensteve@gmail.com>
2024-05-26 10:57:48 -0500

    test: Expanded vec2 tests

--- src/Geo.test.ts
+++ src/Geo.test.ts
@@ -1,7 +1,8 @@
 import { Box, vec, Vec2 } from "./Geo";
 import { expect, jest, test, it } from '@jest/globals';
 
-let dataSet = [
+let vec2DataSet = [
+	[0, 0],
 	[0, 1],
 	[1, 0],
 	[-1, 0],
@@ -11,16 +12,20 @@ let dataSet = [
 ];
 
 let lengthDataSet = [
+	[0, 0, 0],
 	[0, 1, 1],
 	[0, 5, 5],
+	[0, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER],
+	[Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, 12738103345051544],
+	[-1 * Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, 12738103345051544],
 ];
 
-test.each(dataSet)('a vector will have correct dimensions: (%p, %p)', (x, y) => {
+test.each(vec2DataSet)('a vector will have correct dimensions: (%p, %p)', (x, y) => {
 	let v = new Vec2(x, y);
 	expect(v.x).toBe(x);
 	expect(v.y).toBe(y);
 });
-test.each(dataSet)('vec() returns Vec2: (%p, %p)', (x, y) => {
+test.each(vec2DataSet)('vec() returns Vec2: (%p, %p)', (x, y) => {
 	let v = vec(x, y);
 	expect(v).toBeInstanceOf(Vec2);
 });
@@ -29,8 +34,12 @@ test.each(lengthDataSet)('vector lengths are calculated properly: (%p, %p)', (x,
 	let v = vec(x, y);
 	expect(v.length()).toBe(r);
 });
-test.each(dataSet)('vector normalize() will have length 1: (%p, %p)', (x, y) => {
+test.each(vec2DataSet)('Vec2.normalize() will have correct length: (%p, %p)', (x, y) => {
 	let v = vec(x, y);
-	v = v.normalize();
-	expect(v.length()).toBeCloseTo(1);
+	let normal = v.normalize();
+	if (v.length() > 1) {
+		expect(normal.length()).toBeCloseTo(1);
+	} else {
+		expect(normal.length()).toBe(v.length());
+	}
 });
