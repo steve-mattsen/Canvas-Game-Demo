ee6ff385da820b423ad5447f15fb9245b7eb656f
Steve Mattsen <mattsensteve@gmail.com>
2024-05-26 11:15:22 -0500

    test: Added simple box test

--- src/Geo.test.ts
+++ src/Geo.test.ts
@@ -20,7 +20,7 @@ let lengthDataSet = [
 	[-1 * Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, 12738103345051544],
 ];
 
-test.each(vec2DataSet)('a vector will have correct dimensions: (%p, %p)', (x, y) => {
+test.each(vec2DataSet)('a vector will have correct dimensions: (%p, %p)', (x: number, y: number) => {
 	let v = new Vec2(x, y);
 	expect(v.x).toBe(x);
 	expect(v.y).toBe(y);
@@ -43,3 +43,14 @@ test.each(vec2DataSet)('Vec2.normalize() will have correct length: (%p, %p)', (x
 		expect(normal.length()).toBe(v.length());
 	}
 });
+
+let boxDataSet = [
+	[0, 0, 1, 1, 0, 0],
+]
+test.each(boxDataSet)('boxes will have correct dimensions: (%p, %p, %p, %p', (x, y, width, height, ox, oy) => {
+	let b = new Box(x, y, width, height, vec(ox, oy));
+	expect(b.x).toBe(x);
+	expect(b.y).toBe(y);
+	expect(b.width).toBe(width);
+	expect(b.height).toBe(height);
+})
